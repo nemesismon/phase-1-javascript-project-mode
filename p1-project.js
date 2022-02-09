120d2f01c36d01ea75694863dc277b09e751d8e3
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-02-09 10:44:32 -0500

    Project 'Why Am I Only Getting Three Cards???

--- p1-project.js
+++ p1-project.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     async function randomWorks () {
         for (let i=0; i < 4; i++) {
-        // let num = randomNum();
-        // console.log(num);
         fetchData(i);
         }
     }
@@ -13,42 +11,36 @@ document.addEventListener('DOMContentLoaded', function () {
         return parseInt(Math.random() * (63450 - 0) + 1);
     }
     
-    function fetchData (i) {
+    async function fetchData (i) {
         for (let x=0; x < 1; x++) {
         let num = randomNum();
-        console.log(num);
-        fetch (`https://collectionapi.metmuseum.org/public/collection/v1/objects/${num}`)
+        let workObj = await fetch (`https://collectionapi.metmuseum.org/public/collection/v1/objects/${num}`)
         .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            console.log(data.message);
-            console.log(data.additionalImages.length);
-            console.log(data.primaryImageSmall);
-            console.log(i);
-            if (data.message === 'ObjectID not found' || data.primaryImageSmall === '') {
-                x--;
-            }
-            else {
-                rndrWork(data, i);
-            }
-        })  
+        .then((data) => data);
+        if (workObj.message === 'ObjectID not found' || workObj.primaryImageSmall === '') {
+            x--;
+            fetchData();
+        }
+        else {
+            rndrWork(workObj, i);
+        }   
     }}
     
-    function rndrWork(data, i) {
+    function rndrWork(workObj, i) {
         let img = document.createElement('img');
         let title = document.createElement('p');
         let artist = document.createElement('p');
-        let info = document.createElement('p');
-        img.src = data.primaryImageSmall;
-        title.innerText = data.title;
+        // let info = document.createElement('p');
+        img.src = workObj.primaryImageSmall;
+        title.innerText = workObj.title;
         if (title.innerText === '') {
             title.innerText = 'Untitled';
         }
-        artist.innerText = data.artistDisplayName;
+        artist.innerText = workObj.artistDisplayName;
         if (artist.innerText === '') {
             artist.innerText = 'Artist Unknown';
         }
-        // info.innerText = data.
+        // info.innerText = workObj
         document.getElementById(`content${i}`).appendChild(img);
         document.getElementById(`title${i}`).appendChild(title);
         document.getElementById(`artist${i}`).appendChild(artist);
