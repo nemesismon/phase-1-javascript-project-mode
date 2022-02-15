39c366ec0e5ca37b9f222769cba57a80270beb8b
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-02-15 16:29:29 -0500

    Reworked DOM manipulation to include comment box for each work

--- p1-project.js
+++ p1-project.js
@@ -1,65 +1,94 @@
-document.addEventListener('DOMContentLoaded', function () {
-   //Use indexes to preload random numbers, then use functions to check, find new, and overwrite back to the array
+document.addEventListener('DOMContentLoaded', () => {
 
-   //Blog about non-E6 notation for loops or the fetch scheme for bad data
+    fetchData();
+
+    //Comments here;
+    //Pictures clickable;
+
+});
+   //Use indexes to preload random numbers, then use functions to check, find new, and overwrite back to the array;
+   //Blog about non-E6 notation for loops or the fetch scheme for bad data;
    
-    const artIndexes = [];
-    console.log(artIndexes);
+    const artLiked = [];
     
-    function randomNum () {
+    function randomNum() {
             return parseInt(Math.random() * (63450 - 0) + 1);
     }
     
-    function fetchData () {
+    function fetchData() {
             for (let i=0; i < 4; i++) {
-            getFetch(i);
+            getFetch();
             }
     }  
 
-    async function getFetch (i) {
+    function getFetch() {
         let num = randomNum();
-        await fetch (`https://collectionapi.metmuseum.org/public/collection/v1/objects/${num}`)
+        fetch (`https://collectionapi.metmuseum.org/public/collection/v1/objects/${num}`)
         .then((res) => res.json())
         .then((workObj) => {
             if (workObj.message === 'Not a valid object' || workObj === 'ObjectID not found' || workObj.primaryImageSmall === '' || workObj.title === undefined) {
-                getFetch(i);
+                getFetch();
             }
             else {
-                artIndexes[i] = num;
-                console.log(artIndexes[i]);
-                rndrWork(workObj, i);
+                artLiked.push(num);
+                rndrWork(workObj);
             }
         })
     }
     
-    function rndrWork(workObj, i) {
-        let img = document.createElement('img');
-        let title = document.createElement('p');
-        let artist = document.createElement('p');
-        img.src = workObj.primaryImageSmall;
+    function rndrWork(workObj) {
+
+        const container = document.getElementById('container');
+        const artPiece = document.createElement('div');
+        const title = document.createElement('p');
+        const img = document.createElement('img');
+        const artist = document.createElement('p');
+        const commentText = document.createElement('input');
+        const button = document.createElement('button');
+        container.appendChild(artPiece);
         title.innerText = workObj.title;
         if (title.innerText === '') {
             title.innerText = 'Untitled';
         }
+        img.src = workObj.primaryImageSmall;
         artist.innerText = workObj.artistDisplayName;
         if (artist.innerText === '') {
             artist.innerText = 'Artist Unknown';
         }
-        document.getElementById(`content${i}`).appendChild(img);
-        document.getElementById(`title${i}`).appendChild(title);
-        document.getElementById(`artist${i}`).appendChild(artist);
-        let comment = document.getElementById(`post${i}`);
-        comment.addEventListener('click', function () {
-            let commentBox = document.getElementById(`comment-box${i}`).value;  
+        commentText.placeholder = 'Thoughts/comments on piece';
+        commentText.type = 'text';
+        button.innerText = 'Submit';
+        artPiece.appendChild(title);
+        artPiece.appendChild(img);
+        artPiece.appendChild(artist);
+        artPiece.appendChild(commentText);
+        artPiece.appendChild(button);
+        button.addEventListener('click', () => {
+            let text = document.createTextNode(commentText.value);
             let li = document.createElement('li');
-            let text = document.createTextNode(commentBox);
             li.appendChild(text);
-            document.getElementById(`unordered${i}`).appendChild(li);
+            artPiece.appendChild(li);
+            commentText.placeholder = 'Thoughts/comments on piece';
+        });
+        img.addEventListener('click', () => {
+            alert(`You liked ${title.innerText} by ${artist.innerText} the most!`);
+            window.location.reload();
         })
-
-        //Event listener for image liked most here
+        // container.innerHTML += `
+        // <div class='card'>
+        //     <header>
+        //         <h4>${workObj.title}</h4>
+        //     </header>
+        //     <div>
+        //         <h4>${img.src = workObj.primaryImageSmall}</h4>
+        //     </div>
+        //     <footer>
+        //         <h4>${workObj.artist}</h4>
+        //     </footer>
+        //     <input type='text' id='comment-box' placeholder='Thoughts/comments on piece'>
+        //     <button id='post0'>Submit</button>
+        //     <ul id='unordered0'>
+        //     </ul>
+        //     </div>
+        // `
     };
-
-    fetchData();
-    
-});
